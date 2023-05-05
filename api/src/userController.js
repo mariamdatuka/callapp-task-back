fbe7f73628640d8799d0b6bba850e0b4b137f85a
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-05-05 10:11:48 +0400

    create put request

--- api/src/userController.js
+++ api/src/userController.js
@@ -34,3 +34,20 @@ export function deleteItem(req,res){
   fs.writeFileSync("data.json", JSON.stringify(data));
   res.send(item);
 }
+
+export function updateTable(req, res) {
+    const data = JSON.parse(fs.readFileSync("data.json"));
+    const id = req.params.id;
+  
+    const updateData = req.body;
+    const index = data.findIndex((item) => item.id == id);
+    if (index === -1) {
+      return res.status(404).send("Data not found");
+    }
+  
+    data[index] = { ...data[index], ...updateData };
+  
+    fs.writeFileSync("data.json", JSON.stringify(data));
+  
+    res.send(data[index]);
+  }
--- api/src/userRouter.js
+++ api/src/userRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addItem, getFullData,deleteItem} from "./userController.js";
+import { addItem, getFullData,deleteItem, updateTable} from "./userController.js";
 
 const user=Router();
 user.get('/user', getFullData)
 user.post('/add',addItem)
 user.delete('/add/:id', deleteItem)
+user.put('/add/:id', updateTable);
 
 export default user;
